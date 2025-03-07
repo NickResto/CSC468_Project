c39429ea8ad41010621f476e56e00e23bfabb7a9
GarrettCrowner <123480712+GarrettCrowner@users.noreply.github.com>
2025-03-07 10:35:19 -0500

    Create Buttons4.js

---
+++ UI/components/Buttons4.js
@@ -0,0 +1,15 @@
+import { Button, ButtonGroup } from 'react-bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+function Buttons4() {
+  return (
+      <ButtonGroup>
+        <Button variant="primary">Exciting</Button>
+        <Button variant="primary">Heart-Warming</Button>
+        <Button variant="primary">Scary</Button>
+        <Button variant="primary">Sad</Button>
+      </ButtonGroup>
+  );
+}
+
+export default Buttons4; 
