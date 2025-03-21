814d6691c429ef24f22823d34d67d21e0a70f4c5
GarrettCrowner <123480712+GarrettCrowner@users.noreply.github.com>
2025-03-21 14:56:24 -0400

    Create Buttons3.js

---
+++ Project/Client/components/Buttons3.js
@@ -0,0 +1,13 @@
+import { Button, ButtonGroup } from 'react-bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+function Buttons3() {
+  return (
+      <ButtonGroup>
+        <Button variant="primary">Short</Button>
+        <Button variant="primary">Long</Button>
+      </ButtonGroup>
+  );
+}
+
+export default Buttons3; 
