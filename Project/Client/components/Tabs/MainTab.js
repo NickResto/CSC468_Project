dd203298adeff9248aca6d15f56a04f4f6af73ee
GarrettCrowner <123480712+GarrettCrowner@users.noreply.github.com>
2025-03-21 15:10:49 -0400

    Update MainTab.js

--- Project/Client/components/Tabs/MainTab.js
+++ Project/Client/components/Tabs/MainTab.js
@@ -1,5 +1,4 @@
-// src/components/Tabs/MainTab.js
-import React from 'react';
+import React, { useState } from 'react';
 import Buttons1 from '../Buttons1';
 import Buttons2 from '../Buttons2';
 import Buttons3 from '../Buttons3';
@@ -9,6 +8,12 @@ import Searchbar from '../Searchbar';
 import '../styles.css'; 
 
 function MainTab() {
+  const [movies, setMovies] = useState([]);
+
+  const updateMovies = (newMovies) => {
+    setMovies(newMovies);
+  };
+
   return (
     <div>
       <ul style={{ listStyleType: 'none' }}>
@@ -19,8 +24,25 @@ function MainTab() {
         <p><li><Buttons5 /></li></p>
       </ul>
 
-      <Searchbar />
+      <Searchbar updateMovies={updateMovies} />
 
+      <div className="d-flex justify-content-center mt-4">
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <div key={movie.id} className="movie-item mx-2">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                className="img-fluid"
+                style={{ width: "200px", height: "300px" }}
+              />
+              <p>{movie.title}</p>
+            </div>
+          ))
+        ) : (
+          <p>No recommendations yet.</p>
+        )}
+      </div>
       <div className="d-flex justify-content-center mt-4">
         <img src="/Assets/Asset1.jpg" alt="Jaws movie poster" className="img-fluid mx-4 shrinked-image" />
         <img src="/Assets/Asset2.jpg" alt="Jurassic Park movie poster" className="img-fluid mx-4 shrinked-image" />
