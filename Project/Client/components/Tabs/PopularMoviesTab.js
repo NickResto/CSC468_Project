c1e4acf11e8dbda10e3115eaf601929bb61c507e
GarrettCrowner <123480712+GarrettCrowner@users.noreply.github.com>
2025-03-21 14:57:51 -0400

    Create PopularMoviesTab.js

---
+++ Project/Client/components/Tabs/PopularMoviesTab.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+function PopularMoviesTab() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    //fetch("http://localhost:5001/api/movies/popular")
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=ff6ae1ff821d74606dbfcdfec8dd320d")
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results || []);
+      })
+      .catch((error) => console.error("Error fetching movies:", error));
+  }, []);  
+
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">Popular Movies</h2>
+      <div className="row">
+        {movies.map((movie) => (
+          <div key={movie.id} className="col-md-3 mb-4">
+            <div className="card">
+              <img 
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                alt={movie.title} 
+                className="card-img-top" 
+              />
+              <div className="card-body">
+                <h5 className="card-title">{movie.title}</h5>
+                <p className="card-text">{movie.release_date}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default PopularMoviesTab;
