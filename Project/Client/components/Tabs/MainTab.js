022f329eeb87668a8b1f395a993b87f575e96c80
GarrettCrowner <123480712+GarrettCrowner@users.noreply.github.com>
2025-03-21 16:06:54 -0400

    Update MainTab.js

--- Project/Client/components/Tabs/MainTab.js
+++ Project/Client/components/Tabs/MainTab.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
-import Buttons1 from '../Buttons1';
-import Buttons2 from '../Buttons2';
-import Buttons3 from '../Buttons3';
-import Buttons4 from '../Buttons4';
-import Buttons5 from '../Buttons5';
+import MovieButtons from '../MovieButtons';  // Import the consolidated MovieButtons component
 import Searchbar from '../Searchbar';
-import '../styles.css'; 
+import '../styles.css';
 
 function MainTab() {
   const [movies, setMovies] = useState([]);
@@ -17,11 +13,7 @@ function MainTab() {
   return (
     <div>
       <ul style={{ listStyleType: 'none' }}>
-        <p><li><Buttons1 /></li></p>
-        <p><li><Buttons3 /></li></p>
-        <p><li><Buttons2 /></li></p>
-        <p><li><Buttons4 /></li></p>
-        <p><li><Buttons5 /></li></p>
+        <p><li><MovieButtons updateMovies={updateMovies} /></li></p> 
       </ul>
 
       <Searchbar updateMovies={updateMovies} />
@@ -40,9 +32,10 @@ function MainTab() {
             </div>
           ))
         ) : (
-          <p>No recommendations yet.</p>
+          <p></p>
         )}
       </div>
+
       <div className="d-flex justify-content-center mt-4">
         <img src="/Assets/Asset1.jpg" alt="Jaws movie poster" className="img-fluid mx-4 shrinked-image" />
         <img src="/Assets/Asset2.jpg" alt="Jurassic Park movie poster" className="img-fluid mx-4 shrinked-image" />
