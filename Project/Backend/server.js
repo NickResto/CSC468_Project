1761ab2d0afb39b97c47b7fdddeb02428c820f40
GarrettCrowner <123480712+GarrettCrowner@users.noreply.github.com>
2025-03-23 19:05:45 -0400

    Updated server.js
    Added comment statements
    Added closing parentheses
    Removed duplicate code

--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -1,4 +1,7 @@
-<<<<<<< HEAD
+//npm install express
+//npm install cors
+//npm install dotenv
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -17,24 +20,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-=======
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5001;
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
->>>>>>> f119766a1b12c2c0b3e2e9054524b4a3951c84e0
 });
